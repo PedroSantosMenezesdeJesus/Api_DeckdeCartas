2383b4cf43a45d115267aa0acbfde95b5df97702
PedroSantosMenezesdeJesus <pedro.jesus63@etec.sp.gov.br>
2025-11-25 00:13:29 -0300

    salvar

---
+++ flutter_api_cartas/lib/card_class.dart
@@ -0,0 +1,22 @@
+class Cards
+{
+  String? code;
+  String? image;
+  //String? value;
+  //String? suit;
+  
+  Cards({
+    this.code,
+    this.image,
+    //this.value,
+   // this.suit,
+  });
+
+  Cards.empty()
+    : code = '',
+      image = '';
+
+  Cards.fromJson(Map <String, dynamic> data)
+    : code = data['code'] as String?,
+      image = data['image'] as String?;
+}
